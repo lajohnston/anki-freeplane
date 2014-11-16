7298fd7087fc45d5ae8b45fa34ad8b0b7e0ca503
Lee <maxthedog26@hotmail.com>
2014-11-16 11:41:24 +0000

    Find existing note, but dupe check not working

--- freeplane_importer/importer.py
+++ freeplane_importer/importer.py
@@ -3,21 +3,24 @@ from model_not_found_exception import ModelNotFoundException
 class Importer:
 	def __init__(self, collection):
 		self.collection = collection
+		self.model = False
+		self.model_fields = []
 
 
 	def import_note(self, import_data):
+		self.__load_model(import_data['model'])
+
 		# Create note for the relevant model and deck
-		model = self.__load_model(import_data['model'])
-		note = self.collection.newNote()
+		note = self.__find_or_create_note(import_data['id'])
 		note.model()['did'] = self.collection.decks.id(import_data['deck']);
 
 		# If model's first field is 'id', set its value to the node's id
-		field_names = self.collection.models.fieldNames(model)
-		if len(field_names) > 0 and field_names[0].lower() == 'id':
-			note[field_names[0]] = import_data['id']
+		id_field = self.__get_model_id_field()
+		if id_field is not None:
+			note[id_field] = import_data['id']
 
 		# Import each field
-		for field in field_names:
+		for field in self.model_fields:
 			if field in import_data['fields']:
 				note[field] = import_data['fields'][field]
 
@@ -35,5 +38,33 @@ class Importer:
 
 		# Set current model
 		self.collection.models.setCurrent(model)
+		self.model = model
+		self.model_fields = self.collection.models.fieldNames(self.model)
 
-		return model
+
+	def __get_model_id_field(self):
+		if len(self.model_fields) > 0 and self.model_fields[0].lower() == 'id':
+			return self.model_fields[0]
+		else:
+			return None
+
+
+	def __find_or_create_note(self, node_id):
+		note = self.collection.newNote()
+
+		id_field = self.__get_model_id_field()
+		if id_field is not None:		
+			existing_id = self.collection.db.scalar('select id from notes where flds LIKE ? AND mid = ?', str(node_id) +  "\x1f%", self.model['id'])
+			if existing_id is not None:
+				note.id = existing_id
+
+		return note
+
+
+
+	#def __find_card(self, node_id, model_id):
+	#	note_id = this.collection.db.scalar('select id from notes where flds LIKE ? AND mid = ?', node_id +  "\x1f%", model_id)
+	#	if note_id is None:
+	#		return None
+	#	else:
+			

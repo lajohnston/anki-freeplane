4af38a60ac09ce1764682aa17b76156ea028fcd9
lajohnston <lajohnston@live.co.uk>
2020-03-03 20:31:17 +0000

    Update example mindmap to Freeplane 1.7 format Add installation instructions to readme

--- README.md
+++ README.md
@@ -4,13 +4,28 @@ This is a plugin for the Anki flashcard system that allows you to convert Freepl
 
 The plugin is currently a work in progress. The backend is working with unit tests, but does not yet have a user interface.
 
+## Installation and example
+
+This is just a proof-of-concept so far and only imports the mindmap found in the example directory. You can change this to point to a mindmap of your choice by editing the [mindmap.py](mindmap.py) file.
+
+1. Copy this repository into the add-on directory (in Anki, click Tools > Add-ons > View files)
+2. Restart Anki. You should now see 'Import mindmap' in the tools menu
+
+If you are importing the example mindmap you might see an error notifying you that `FreeplaneModel` doesn't exist. You need to create this model in Anki with the fields specified in the mindmap attributes ('Front', 'Back').
+
+If imported successfully you should see that a `FreeplaneDeck` deck has been created for you. This is specified in the mindmap in the node with the 'anki:deck' attribute. Inside this deck you will see that 2 cards have been created.
+
+If you import again you'll notice that duplicate cards get created. To prevent this happening, edit the `FreeplaneModel` model to include a field named 'ID' and place it at the top. Delete the existing notes then import again. You'll notice that the ID gets populated with the Freeplane ID for that node, and subsequent imports won't import those cards again. If you edit the text for those nodes in Freeplane and re-import, the existing cards will be updated with the new text so long as the ID hasn't changed.
+
+## Usage
+
 ## Attributes
 
 Add the following attributes to the relevant mindmap nodes (using Alt + F9) to ensure they get imported correctly. An example mindmap can be found in the /examples directory.
 
 ### anki:deck
 
-The name of the deck where ancestor nodes will be imported into.
+The name of the deck where descendant nodes will be imported into.
 
 ### anki:model
 
@@ -24,4 +39,10 @@ Identifies a field within the model that the text node will be imported into.
 
 ### anki:modify
 
-This optional attributes modifies or replaces the text of field before importing it. Mindmap nodes often have abbreviated text that might not give enough information for a flashcard, so this attribute can be used to add additional text. If the modify value contains an asterisk (\*) character, then the original text will be inserted at this point.
+This optional attribute modifies or replaces the text of the field before importing it. Mindmap nodes often have abbreviated text that might not give enough information for a flashcard, so this attribute can be used to replace the text with something else when it gets imported. If the modified value contains an asterisk (\*) character, then the original text will be inserted at this point.
+
+## Development
+
+Run unit tests with `python -m unittest`.
+
+The next step for this plugin will be creating a GUI so you can select the mindmap(s) you wish to import.
--- example/ExampleMindmap.mm
+++ example/ExampleMindmap.mm
@@ -1,22 +1,26 @@
-<map version="freeplane 1.3.0">
+<map version="freeplane 1.7.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Import test" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1415304958179"><hook NAME="MapStyle">
-    <properties show_icon_for_attributes="true"/>
+<node TEXT="Import test" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1583261765174"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false"/>
 
 <map_styles>
-<stylenode LOCALIZED_TEXT="styles.root_node">
-<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
-<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
 <font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.note"/>
 <stylenode LOCALIZED_TEXT="defaultstyle.floating">
 <edge STYLE="hide_edge"/>
 <cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
@@ -30,7 +34,7 @@
 <icon BUILTIN="yes"/>
 </stylenode>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
 <font SIZE="18"/>
 </stylenode>
@@ -50,19 +54,19 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
-<node TEXT="Deck" POSITION="right" ID="ID_793372572" CREATED="1415464979694" MODIFIED="1415464998957">
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Deck" POSITION="right" ID="ID_793372572" CREATED="1415464979694" MODIFIED="1583262198781">
 <edge COLOR="#00ff00"/>
-<attribute NAME="anki:deck" VALUE="PluginTest"/>
-<node TEXT="This is the front" ID="ID_1194397559" CREATED="1415304958826" MODIFIED="1415464990402">
-<attribute NAME="anki:model" VALUE="FreeplaneTest"/>
+<attribute NAME="anki:deck" VALUE="FreeplaneDeck"/>
+<node TEXT="This is the front" ID="ID_1194397559" CREATED="1415304958826" MODIFIED="1583262144468">
+<attribute NAME="anki:model" VALUE="FreeplaneModel"/>
 <attribute NAME="anki:field" VALUE="Front"/>
 <node TEXT="This is the back" ID="ID_624938500" CREATED="1415304964333" MODIFIED="1415305728686">
 <attribute NAME="anki:field" VALUE="Back"/>
 </node>
 </node>
-<node TEXT="This is the front of another card" ID="ID_807505283" CREATED="1415304958826" MODIFIED="1415465022376">
-<attribute NAME="anki:model" VALUE="FreeplaneTest"/>
+<node TEXT="This is the front of another card" ID="ID_807505283" CREATED="1415304958826" MODIFIED="1583262150135">
+<attribute NAME="anki:model" VALUE="FreeplaneModel"/>
 <attribute NAME="anki:field" VALUE="Front"/>
 <node TEXT="This is the back" ID="ID_365503531" CREATED="1415304964333" MODIFIED="1415305728686">
 <attribute NAME="anki:field" VALUE="Back"/>

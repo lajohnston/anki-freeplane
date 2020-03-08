746e3dd714653df428f0541609b9c51e29cd2726
lajohnston <lajohnston@live.co.uk>
2020-03-08 15:10:28 +0000

    Tidy test_node.py

--- spec/test_node.py
+++ spec/test_node.py
@@ -11,7 +11,7 @@ class TestNode(unittest.TestCase):
         self.root = etree.fromstring("<map></map>")
         self.next_id = 0
 
-    def __add_node(self, parent, text, attributes={}):
+    def __add_node(self, parent, attributes={}, text=''):
         self.next_id += 1
         string = '<node TEXT="' + text + '" ID="ID_' + str(self.next_id) + '">'
 
@@ -24,28 +24,27 @@ class TestNode(unittest.TestCase):
         return node
 
     def test_it_should_extract_the_deck_from_the_nearest_ancestor_of_the_node(self):
-        node1 = self.__add_node(self.root, 'Root node', {
-                                'anki:deck': 'root_deck'})
-        node2 = self.__add_node(node1, 'Blah', {'anki:deck': 'parent_deck'})
-        node3 = self.__add_node(node2, 'Blah')
+        node1 = self.__add_node(self.root, {'anki:deck': 'root_deck'})
+        node2 = self.__add_node(node1, {'anki:deck': 'parent_deck'})
+        node3 = self.__add_node(node2)
         fp_node = Node(self.root, node3)
 
         self.assertEqual('parent_deck', fp_node.get_deck())
 
     def test_it_should_extract_the_model_from_the_anki_model_attribute(self):
-        node = self.__add_node(self.root, 'Root node', {'anki:model': 'Basic'})
+        node = self.__add_node(self.root, {'anki:model': 'Basic'})
         fp_node = Node(self.root, node)
 
         self.assertEqual('Basic', fp_node.get_model())
 
     def test_it_should_extract_the_id_from_the_node(self):
-        node = self.__add_node(self.root, 'Root node', {'anki:model': 'Basic'})
+        node = self.__add_node(self.root, {'anki:model': 'Basic'})
         fp_node = Node(self.root, node)
 
         self.assertEqual('ID_1', fp_node.get_node_id())
 
     def test_it_should_extract_fields_from_the_anki_field_wildcard_attribute(self):
-        node = self.__add_node(self.root, 'Foo', {'anki:field:Bar': 'Baz'})
+        node = self.__add_node(self.root, {'anki:field:Bar': 'Baz'})
         fp_node = Node(self.root, node)
 
         fields = fp_node.get_fields()
@@ -55,7 +54,7 @@ class TestNode(unittest.TestCase):
 
     def test_it_should_replace_the_wildcard_character_with_the_node_text(self):
         node = self.__add_node(
-            self.root, 'Foo', {'anki:field:Foo': 'Bar * Baz'})
+            self.root, {'anki:field:Foo': 'Bar * Baz'}, 'Foo')
         fp_node = Node(self.root, node)
 
         fields = fp_node.get_fields()
@@ -65,28 +64,26 @@ class TestNode(unittest.TestCase):
 
     def test_it_should_extract_the_node_text(self):
         text = 'This is the node text'
-        node = self.__add_node(self.root, text, {'anki:field': 'Name'})
+        node = self.__add_node(self.root, {'anki:field': 'Name'}, text)
         fp_node = Node(self.root, node)
         self.assertEqual(text, fp_node.get_text())
 
     def test_it_should_extract_fields_from_child_nodes(self):
-        parent = self.__add_node(self.root, '', {
+        parent = self.__add_node(self.root, {
                                  'anki:field:Front': 'This is the front'})
-        self.__add_node(parent, '', {
-                                'anki:field:Back': 'This is the back'})
+        self.__add_node(parent, {
+            'anki:field:Back': 'This is the back'})
         fp_node = Node(self.root, parent)
         fields = fp_node.get_fields()
 
         self.assertEqual('This is the back', fields['Back'])
 
     def test_it_should_extract_fields_from_nested_child_nodes(self):
-        parent = self.__add_node(self.root, '', {
+        parent = self.__add_node(self.root, {
                                  'anki:field:Front': 'This is the front'})
-        middle = self.__add_node(
-            parent, 'This does not define a field, but it has a child that does')
 
-        self.__add_node(middle, '', {
-                                'anki:field:Back': 'This is the back'})
+        middle = self.__add_node(parent)
+        self.__add_node(middle, {'anki:field:Back': 'This is the back'})
 
         fp_node = Node(self.root, parent)
         fields = fp_node.get_fields()
@@ -94,11 +91,10 @@ class TestNode(unittest.TestCase):
         self.assertEqual('This is the back', fields['Back'])
 
     def test_it_should_not_extract_fields_from_sub_notes(self):
-        parent = self.__add_node(self.root, 'This is the parent node')
-        sub_note = self.__add_node(parent, 'This is another note', {
-                                   'anki:model': 'Basic'})
+        parent = self.__add_node(self.root)
+        sub_note = self.__add_node(parent, {'anki:model': 'Basic'})
         self.__add_node(
-            sub_note, '', {'anki:field:SubNote': 'This should be ignored'})
+            sub_note, {'anki:field:SubNote': 'This should be ignored'})
 
         fp_node = Node(self.root, parent)
         fields = fp_node.get_fields()
